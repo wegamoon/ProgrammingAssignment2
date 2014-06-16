0ec321d2616a0b9974fc42e6ec2a91dfdc6a73c2
Wataru Uegami <w.wegami@gmail.com>
2014-06-16 22:17:23 +0900

    first answer

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Following two functions calculates inversed matrix and save in a cache
+## in preparation for next use.
 
-## Write a short comment describing this function
+## It creates a list of functions and posesses the result of inversed matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        r_mat <- NULL
+        set <- function(y){
+                x <<- y
+                r_mat <<- NULL
+        }
+        get <- function()       x
+        setMat <- function(revMat)      r_mat <<- revMat
+        getMat <- function()   r_mat
+        list(set = set, get = get, 
+             setMat = setMat,
+             getMat = getMat)
 }
 
 
-## Write a short comment describing this function
+## If the inversed matrix has already calculated, the result will be passed 
+## from "cache" with a comment 'getting cached data'
+## otherwise, it calculate inversed matrix using function 'solve()'.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        r <- x$getMat()
+        if(!is.null(r)){
+                message("getting cached data")
+                return(r)
+        }
+        m <- x$get()
+        r <- solve(m, ...)
+        x$setMat(r)
+        r
 }
